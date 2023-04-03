424703ea1d2f8738d86f0817329bc79f171ee536
G S  Anandini <110148410+GSAnandini@users.noreply.github.com>
2023-04-03 19:24:38 +0530

    Update views.py

--- quickstart/views.py
+++ quickstart/views.py
@@ -1,54 +1,56 @@
 from django.shortcuts import render
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
 from .models import *
 from .serializers import *
 from rest_framework import status
+from rest_framework.permissions import IsAuthenticated
+from rest_framework_simplejwt.tokens import RefreshToken
+from django.contrib.auth.models import User
+from django.shortcuts import redirect
+from django.urls import reverse
+
+@api_view(['POST'])
+def registerlibrary(request):
+    serializer = LibrarySerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data, status=201)
+    return Response(serializer.errors, status=400)
 
 
-# READ operation
 @api_view(['GET'])
-def home(request):
-    student_obj=Student.objects.all()
-    serializer=StudentSerializer(student_obj,many=True)
-    return Response(serializer.data)
-
-# READ operation for books
-@api_view(['GET'])
+@permission_classes([IsAuthenticated])
 def book_lists(request):
     book_obj = Books.objects.all()
     serializer = BooksSerializer(book_obj, many=True)
     return Response(serializer.data)
 
-# READ operation for library
-@api_view(['GET'])
-def library_lists(request):
-    library_obj = Library.objects.all()
-    serializer = LibrarySerializer(library_obj, many=True)
-    return Response(serializer.data)
 
-# CREATE operation
 @api_view(['POST'])
-def post_student(request):
-    student_obj = Student.objects.filter(name=request.data['name'], email=request.data['email'],password=request.data['password'] )
-    serializer = StudentSerializer(data=request.data)
+def register(request):
+    serializer = UserSerializer(data=request.data)
     if serializer.is_valid():
-        if student_obj.exists():
-            return Response("You are already a registered user!! ", status=status.HTTP_400_BAD_REQUEST)
-        serializer.save()
-        return Response("Your registration is successfull !! ", status=status.HTTP_201_CREATED)
+        user = serializer.save()
+        refresh = RefreshToken.for_user(user)
+        return Response({'refresh': str(refresh), 'access': str(refresh.access_token), 'message': 'User registered successfully'}, status=status.HTTP_201_CREATED)
     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-# UPDATE operation
 @api_view(['POST'])
-def update_student(request,id):
-    student_obj=Student.objects.get(id=id)
-    serializer=StudentSerializer(instance=student_obj,data=request.data)
-    if serializer.is_valid():
-        serializer.save()
-    return Response(serializer.data)
+def login(request):
+    email = request.data.get('email')
+    password = request.data.get('password')
+    user = User.objects.filter(email=email).first()
+    if user is None:
+        return Response({'error': 'Invalid Credentials'})
+    if not user.check_password(password):
+        return Response({'error': 'Invalid Credentials'})
+    refresh = RefreshToken.for_user(user)
+    response = redirect(reverse('registerlibrary'))
+    response.set_cookie(key='access_token', value=str(refresh.access_token))
+    return response
 
-# UPDATE operation for library
+
 @api_view(['POST'])
 def update_library(request,id):
     library_obj=Library.objects.get(id=id)
@@ -56,10 +58,3 @@ def update_library(request,id):
     if serializer.is_valid():
         serializer.save()
     return Response("Data is updated!! ")
-
-# DELETE operation
-@api_view(['DELETE'])
-def delete_student(request,id):
-    student_obj=Student.objects.get(id=id)
-    student_obj.delete()
-    return Response("Student is deleted! ")
